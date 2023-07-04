28280da1d874ef4a68f8c359406ada95c6b6b301
davidblee07 <93348233+davidblee07@users.noreply.github.com>
2023-07-04 18:26:04 -0300

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+    console.log('Ready')
+
+    //  Busque a data atual e atualize-a no DOM
+    var date = new Date()
+    var display_date = date.toLocaleDateString()
+
+    $('#date').text("Data: " + display_date)
+    // Escreva um evento, quando o botão Enviar for clicado
+    $('#button').click(function(){
+
+        //  Obtenha o valor do texto da área de texto usando o método 'val()'
+        let text_value = $('#text').val()
+
+        //  Converta-o em um objeto JS.
+        //  Forneça uma "chave" aqui e escreva o mesmo no arquivo app.py também para extrair dados
+        let input_text = {'/review' : text_value}
+        console.log(input_text)
+
+        //  requisição ajax
+        $.ajax({
+
+            //  tipo da requisição web
+            type : 'POST',
+            url: "/predict",
+
+            //  dados a serem enviados no formato JSON
+            data : JSON.stringify(input_text),
+
+            //  o tipo de resposta esperado é json
+            dataType : 'json',
+
+            //  contentType
+            contentType : 'application/json',
+
+            //  se tudo funcionar, execute esta função
+            success : function(result){
+
+                // extraia previsão e a URL do emoticon do resultado
+                let prediction = result.prediction
+                let emoji_url = result.url 
+
+                //  atualize os elementos DOM
+                console.log(emoji_url)
+
+                //  exiba-os
+                $('#sentiment').text(prediction)
+                $('#sentiment').show()
+
+                $('#emoji').attr('src' , emoji_url)
+                $('#emoji').show()
+            },
+
+            //  se houver algum erro, execute esta função
+            error : function(result){
+
+                console.log(result)
+            }
+        })
+
+
+        //  limpando a caixa de texto após cada pressionamento de botão
+        $('#text').val("")
+    })
+        
+})
